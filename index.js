b1548be2b1ccfa30a90b17852a61e33a19642184
PRASADH03 <kjprasadh03@gmail.com>
2025-06-13 10:19:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,116 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", (e) => {
+      if (e.code === "Enter"|| e. keyCode === 13) {
+        let input = inputField.value;
+        inputField.value = "";
+        output(input);
+      }
+    });
+  });
+  
+  function output(input) {
+    let product;
+  
+    // Regex remove non word/space chars
+    // Trim trailing whitespce
+    // Remove digits - not sure if this is best
+    // But solves problem of entering something like 'hi1'
+  
+    let text = input.toLowerCase();
+    text = text
+      .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "")
+      .replace(/what is/g,"")
+      .replace(/please /g, "")
+      .replace(/ please/g, "")
+      .replace(/are you/g, "")
+      .replace(/who /g,"")
+      .replace(/ is /g,"")
+      .replace(/ tell /g,"")
+      .replace(/ me /g,"");
+    text = text.replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+    console.log(text);
+  
+    if (compare(prompts, replies, text)) { 
+      // Search for exact match in `prompts`
+      product = compare(prompts, replies, text);
+    } else if (text.match(/thank/gi)) {
+      product = "You're welcome!"
+    } else if (text.match(/(executive|director|ed|executive director)/gi)) {
+      // If no match, check if message contains `coronavirus`
+      product = ed[Math.floor(Math.random() * ed.length)];
+    }
+    else if (text.match(/(principal|princi|pri)/gi)) {
+      // If no match, check if message contains `coronavirus`
+      product = princi[Math.floor(Math.random() * princi.length)];
+    }
+    else if (text.match(/(venue|place|location|locate|where)/gi)) {
+      // If no match, check if message contains `coronavirus`
+      product = venue[Math.floor(Math.random() * venue.length)];
+    }  
+    else if (text.match(/(time|time?|exact|time|correct)/gi)) {
+      // If no match, check if message contains `coronavirus`
+      product = time[Math.floor(Math.random() * time.length)];
+    }  else {
+      // If all else fails: random alternative
+      product = alternative[Math.floor(Math.random() * alternative.length)];
+    }
+  
+    // Update DOM
+    addChat(input, product);
+  }
+  
+  function compare(promptsArray, repliesArray, string) {
+    let reply;
+    let replyFound = false;
+    for (let x = 0; x < promptsArray.length; x++) {
+      for (let y = 0; y < promptsArray[x].length; y++) {
+        if (promptsArray[x][y] === string) {
+          let replies = repliesArray[x];
+          reply = replies[Math.floor(Math.random() * replies.length)];
+          replyFound = true;
+          // Stop inner loop when input value matches prompts
+          break;
+        }
+      }
+      if (replyFound) {
+        // Stop outer loop when reply is found instead of interating through the entire array
+        break;
+      }
+    }
+    return reply;
+  }
+  
+  function addChat(input, product) {
+    const messagesContainer = document.getElementById("messages");
+  
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.className = "user response";
+    userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+    messagesContainer.appendChild(userDiv);
+  
+    let botDiv = document.createElement("div");
+    let botImg = document.createElement("img");
+    let botText = document.createElement("span");
+    botDiv.id = "bot";
+    botImg.src = "bot.avif";
+    botImg.className = "avatar";
+    botDiv.className = "bot response";
+    botText.innerText = "Typing...";
+    botDiv.appendChild(botText);
+    botDiv.appendChild(botImg);
+    messagesContainer.appendChild(botDiv);
+    // Keep messages at most recent
+    messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+  
+    // Fake delay to seem "real"
+    setTimeout(() => {
+      botText.innerText = `${product}`;
+      textToSpeech(product)
+    }, 2000
+    )
+  
+  }
